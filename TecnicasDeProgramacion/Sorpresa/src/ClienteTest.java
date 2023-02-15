09f71b77194ed72b50e4eb35bcec4843f087ea61
Diego <diegocarrasco1013@gmail.com>
2023-02-15 16:01:47 +0100

    Actu

--- TecnicasDeProgramacion/Sorpresa/src/ClienteTest.java
+++ TecnicasDeProgramacion/Sorpresa/src/ClienteTest.java
@@ -1,4 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class ClienteTest {
-  
+
+class ClienteTest extends DialogoMac {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        Cliente.main(null);
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testMain() {
+    }
 }
