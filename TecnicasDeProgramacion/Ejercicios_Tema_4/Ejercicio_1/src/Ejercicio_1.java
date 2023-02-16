69afcdc4b61d4d351536708ae3c26a8306f8fb5d
Diego <diegocarrasco1013@gmail.com>
2023-02-16 22:26:17 +0100

    Files

--- TecnicasDeProgramacion/Ejercicios_Tema_4/Ejercicio_1/src/Ejercicio_1.java
+++ TecnicasDeProgramacion/Ejercicios_Tema_4/Ejercicio_1/src/Ejercicio_1.java
@@ -3,5 +3,10 @@ public class Ejercicio_1 {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        //Algoritmo primos
+        int numero = 0;
+        int contador = 0;
+        int i = 0;
+
     }
 }
